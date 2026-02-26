e1adadf4437d83490500f5bafde2ec5838101f57
Ali Mustadji <101158664+L1mus@users.noreply.github.com>
2026-02-26 11:55:42 +0700

    Add account management and password update logic
    Implement account object and password update function with masking.

---
+++ Minitask-1.js
@@ -0,0 +1,24 @@
+const account = {
+  id:32141235,
+  username:"John Doe",
+  password:"RWEQFdsfewfedf32D5W1sdD"
+}
+
+function updateAcc (newPass){
+  if(newPass.length < 12){
+    console.log(`Password anda minimal 12 karakter`)
+  }
+  const updated ={
+    ...account,
+    password:newPass
+  }
+  cutPassword = updated.password.slice(-3)
+  let sensorPassword = updated.password.slice(0,updated.password.length-3).replace(/[a-z0-9]/g,"*")
+  console.log(sensorPassword)
+  return console.log(`
+  Username : ${updated.username}
+  Password : ${sensorPassword}${cutPassword}
+  `)
+}
+
+updateAcc("321542r34rwe")
